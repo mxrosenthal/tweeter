58acc2e47c1ff07f6034eeb00ccdb8e1adddf0ab
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-28 17:43:29 +0000

    implemented character counter. some minor css in the .js files. will update this soon.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  let maxChar = 140;
+  $('textarea').keypress(function() {
+    let lengthInput = $(this).val().length + 1;
+    let lengthLeft = maxChar - lengthInput;
+
+    //traverses the DOM from 'this' up to this' parent, then searches the children for
+    //a child with class '.counter', then inputs the value of lengthLeft into the text field.
+    $(this)
+      .parent()
+      .children('.counter')
+      .text(lengthLeft);
+
+    if (lengthLeft < 0) {
+      $(this)
+        .parent()
+        .children('.counter')
+        .css('color', 'red');
+    }
+  });
+});
