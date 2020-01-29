64f4c8cdad958bfda5c67bb9c0a59f3efb09074e
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-29 23:37:13 +0000

    refetch tweets complete. newly entered tweets are appended to the top of the stack. working state

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  user: {
-    name: 'Newton',
-    avatars: 'https://i.imgur.com/73hZDYK.png',
-    handle: '@SirIsaac'
-  },
-  content: {
-    text: 'If I have seen further it is by standing on the shoulders of giants'
-  },
-  created_at: 1461116232227
-};
+$(document).ready(function() {
+  const createTweetElement = function(obj) {
+    console.log('obj: ', obj);
+    const username = obj.user.name;
+    const avatar = obj.user.avatars;
+    const handle = obj.user.handle;
+    const input = obj.content.text;
+    const datePosted = new Date(obj.created_at);
+    const currentDate = new Date();
+    const timeSince = currentDate - datePosted;
+    const secondsSincePost = Math.floor((timeSince / 86400000) * 86400);
+    const minutesSincePost = Math.floor((timeSince / 86400000) * 1440);
+    const hoursSincePost = Math.floor((timeSince / 86400000) * 24);
+    const daysSincePost = Math.floor(timeSince / 86400000); //number of milliseconds per day.
+    const yearsSincePost = Math.floor(timeSince / 86400000 / 365);
 
-const createTweetElement = function(obj) {
-  const username = obj.user.name;
-  const avatar = obj.user.avatars;
-  const handle = obj.user.handle;
-  const input = obj.content.text;
-  const datePosted = new Date(obj.created_at);
-  const currentDate = new Date();
-  const timeSince = currentDate - datePosted;
-  const daysSincePost = Math.floor(timeSince / 86400000); //number of milliseconds per day.
-  const yearsSincePost = Math.floor((timeSince / 86400000) * 365);
+    const $tweet = $('<article>').addClass('tweet');
+    const $header = $('<header>').addClass('posted-tweet-header');
+    const $div = $('<div>').addClass('input-tweet-body');
+    const $footer = $('<footer>').addClass('posted-tweet-footer');
 
-  const $tweet = $('<article>').addClass('tweet');
-  const $header = $('<header>').addClass('posted-tweet-header');
-  const $div = $('<div>').addClass('input-tweet-body');
-  const $footer = $('<footer>').addClass('posted-tweet-footer');
+    if (input.length > 140) {
+      return;
+    }
 
-  $header.append(`
+    $header.append(`
     <div class="header-logo">
     <img class="avatar-small" src=${avatar} />
     <span class="user-name-small">
@@ -44,78 +41,115 @@ const createTweetElement = function(obj) {
     </span>
 `);
 
-  $div.append(`${input}`);
-  // $footer.append(`Posted: ${daysSincePost} ago.`);
+    $div.append(`${input}`);
+    // $footer.append(`Posted: ${daysSincePost} ago.`);
 
-  if (yearsSincePost > 0) {
-    $footer.append(`Posted ${yearsSincePost} years ago.
+    if (yearsSincePost > 0) {
+      $footer.append(`Posted ${yearsSincePost} years ago.
   <div class="glyph">
     <i class="fas fa-flag"></i>
     <i class="fas fa-retweet"></i>
     <i class="far fa-heart"></i>
   </div>`);
-  } else {
-    $footer.append(`Posted ${daysSincePost} days ago.
+    } else if (daysSincePost > 0) {
+      $footer.append(`Posted ${daysSincePost} days ago.
     <div class="glyph">
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
       <i class="far fa-heart"></i>
     </div>`);
-  }
+    } else if (hoursSincePost > 0) {
+      $footer.append(`Posted ${hoursSincePost} hours ago.
+    <div class="glyph">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+    </div>`);
+    } else if (minutesSincePost > 0) {
+      $footer.append(`Posted ${minutesSincePost} minutes ago.
+    <div class="glyph">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+    </div>`);
+    } else if (secondsSincePost > 0) {
+      $footer.append(`Posted ${secondsSincePost} seconds ago.
+    <div class="glyph">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+    </div>`);
+    } else {
+      $footer.append(`Posted just now.
+    <div class="glyph">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+    </div>`);
+    }
 
-  $tweet.append($header);
-  $tweet.append($div);
-  $tweet.append($footer);
+    $tweet.append($header, $div, $footer);
 
-  return $tweet;
-};
+    return $tweet;
+  };
 
-const renderTweets = function(tweets) {
-  // let arrayOfTweets = [];
-  for (const tweet of tweets) {
-    const newTweet = createTweetElement(tweet); //needs to be an obj?
-    $('#tweet-container').append(newTweet);
-  }
-  // $('.tweet-container').append()
-  // return arrayOfTweets;
-};
+  const renderTweets = function(tweets) {
+    const container = $('#tweet-container');
+    container.empty();
 
-const data = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac'
-    },
-    content: {
-      text:
-        'If I have seen further it is by standing on the shoulders of giants'
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd'
-    },
-    content: {
-      text: 'Je pense , donc je suis'
-    },
-    created_at: 1461113959088
-  }
-];
+    for (const tweet of tweets) {
+      const newTweet = createTweetElement(tweet); //needs to be an obj?
+      container.prepend(newTweet);
+    }
+  };
 
-const $tweet = createTweetElement(tweetData);
+  // const renderLastTweet = function(tweets) {
+  //   const tweet = tweets.slice(-1);
+  //   const container = $('#tweet-container');
+  //   container.prepend(tweet);
+  // };
 
-// console.log(renderTweets(data));
-// console.log('arrayOfTweets: ', arrayOfTweets);
+  //POST tweet TO the SERVER
+  const $form = $('#submit-tweet');
+  $form.on('submit', function(event) {
+    event.preventDefault();
+    // console.log('form:', $form.serialize());
 
-// Test / driver code (temporary)
-// console.log('$tweet:', $tweet); // to see what it looks like
+    const inputText = $('#newTweetTextArea').val();
+    console.log('inputText: ', inputText);
+    if (inputText.length > 140) {
+      alert('Your message is too long.');
+    } else if (inputText.length === 0) {
+      alert("Can't submit empty Tweet.");
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: $form.serialize(),
+        success: data => {
+          // $('#tweet-container').prepend(createTweetElement(data));
+          loadTweets();
+          console.log('data: ', data);
+          $('#newTweetTextArea')
+            .val('')
+            .focus();
+        }
+      });
+    }
+  });
 
-$(document).ready(function() {
-  // $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  //GET tweet FROM the SERVER
+  const loadTweets = function() {
+    console.log('loading tweets');
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      data: $form.serialize(),
+      success: post => {
+        renderTweets(post);
+      }
+    });
+  };
 
-  renderTweets(data);
+  loadTweets();
 });
