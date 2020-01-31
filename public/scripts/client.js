8c1bbe160cf8ba97412e665f0b6f7e76e4a1ccd6
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-31 03:13:52 +0000

    cleaning up code prior to submitting. working state.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,6 @@
 
 $(document).ready(function() {
   const createTweetElement = function(obj) {
-    console.log('obj: ', obj);
     const username = obj.user.name;
     const avatar = obj.user.avatars;
     const handle = obj.user.handle;
@@ -38,54 +37,54 @@ $(document).ready(function() {
     </div>
     <span class="user-handle-small">
     ${handle}
-    </span>
-`);
+    </span>`);
 
+    //appending input from the
     $div.append(`${input}`);
-    // $footer.append(`Posted: ${daysSincePost} ago.`);
 
+    //determining time since post to update relative time since post was posted.
     if (yearsSincePost > 0) {
       $footer.append(`Posted ${yearsSincePost} years ago.
-  <div class="glyph">
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="far fa-heart"></i>
-  </div>`);
+      <div class="glyph">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+      </div>`);
     } else if (daysSincePost > 0) {
       $footer.append(`Posted ${daysSincePost} days ago.
-    <div class="glyph">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="far fa-heart"></i>
-    </div>`);
+      <div class="glyph">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+      </div>`);
     } else if (hoursSincePost > 0) {
       $footer.append(`Posted ${hoursSincePost} hours ago.
-    <div class="glyph">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="far fa-heart"></i>
-    </div>`);
+      <div class="glyph">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+      </div>`);
     } else if (minutesSincePost > 0) {
       $footer.append(`Posted ${minutesSincePost} minutes ago.
-    <div class="glyph">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="far fa-heart"></i>
-    </div>`);
+      <div class="glyph">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+      </div>`);
     } else if (secondsSincePost > 0) {
       $footer.append(`Posted ${secondsSincePost} seconds ago.
-    <div class="glyph">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="far fa-heart"></i>
-    </div>`);
+      <div class="glyph">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+      </div>`);
     } else {
       $footer.append(`Posted just now.
-    <div class="glyph">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="far fa-heart"></i>
-    </div>`);
+      <div class="glyph">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+      </div>`);
     }
 
     $tweet.append($header, $div, $footer);
@@ -123,19 +122,16 @@ $(document).ready(function() {
         .focus('#newTweetTextArea');
       let tooLong = true;
     } else if (inputText.length === 0) {
-      // $('#submit-tweet').click(function() {
       $('#noText')
         .slideDown('slow')
         .focus('#newTweetTextArea');
       let noText = true;
-      // });
     } else {
       $.ajax({
         method: 'POST',
         url: '/tweets',
         data: $form.serialize(),
         success: data => {
-          // $('#tweet-container').prepend(createTweetElement(data));
           loadTweets();
           console.log('data: ', data);
           $('#newTweetTextArea')
@@ -167,11 +163,28 @@ $(document).ready(function() {
     });
   };
 
-  loadTweets();
+  loadTweets(); //loads all tweets in memore when you enter the site.
 
+  let tweetBox = false;
   $('#double-down').click(function() {
-    $('#new-tweet').slideDown('slow');
-
-    $('#newTweetTextArea').focus();
+    if (tweetBox) {
+      //if tweetBox is open
+      $('#new-tweet').slideUp('slow'); //it will close
+      tweetBox = false;
+    } else {
+      //if tweetBox is closed
+      $('#new-tweet').slideDown('slow'); //it will open
+      $('#newTweetTextArea').focus();
+      tweetBox = true;
+      if (tooLong) {
+        //if there are error messages, slide them out of view if newtweet arrow is toggled.
+        $('#tooLong').slideUp('slow');
+        $('#newTweetTextArea').val('');
+        $('.counter').text(140);
+      }
+      if (noText) {
+        $('#noText').slideUp('slow');
+      }
+    }
   });
 });
